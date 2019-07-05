b9d036a2d942c34dd44d1162e3daee4924318874
solaris6 <solaris6@gmail.com>
2019-07-05 09:48:08 +0300

    test

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 from pathlib import Path
 
 if __name__ == '__main__':
+    print('[deploy] Deploy site...')
     PATHFILE_home_user_deploy_site_deploy_site_py = Path(sys.argv[0])
     PATHDIR_home_user_deploy_site = PATHFILE_home_user_deploy_site_deploy_site_py.parent
     PATHDIR_home_user = PATHDIR_home_user_deploy_site.parent
@@ -17,43 +18,83 @@ if __name__ == '__main__':
         'getrs': 'rs',
         'getfw': 'fw',
         'getln': 'Ln',
-        'ynsight': 'ynsight'
+        'ynsight': 'ynsight',
+        'getprojekt': 'project'
     }[USER]
 
-    site = 'site_' + project
-    PATHDIR_site = PATHDIR_home_user / site
+    sitepackage = 'site_' + project
+    URL_site = USER + '.pythonanywhere.com'
+    PATHDIR_sitepackage = PATHDIR_home_user / sitepackage
 
-    URL_repository = '''https://github.com/solaris6/%project%.git'''\
+    URL_projectrepository = '''https://github.com/solaris6/%project%.git'''\
         .replace('%project%', project)
-    PATHDIR_repository = PATHDIR_home_user / project
-    PATHDIR_repository_site = PATHDIR_repository / '_site' / site
+
+    PATHDIR_repositories = PATHDIR_home_user / 'repositories'
+    PATHDIR_projectrepository = PATHDIR_repositories / project
+    PATHDIR_projectrepository_sitepackage = PATHDIR_projectrepository / '_site' / sitepackage
 
     PATHFILE_wsgi_py = Path(
         '/var/www/%USER%_pythonanywhere_com_wsgi.py'
             .replace('%USER%', USER)
     )
 
-    print('test0')
-    print(sys.argv)
-    print(__file__)
-    print(PATHFILE_home_user_deploy_site_deploy_site_py)
-    print(PATHDIR_home_user_deploy_site)
-    print(PATHDIR_home_user)
-    print(USER)
-    print(project)
-    print(site)
-    print(PATHDIR_site)
-    print(URL_repository)
-    print(PATHDIR_repository)
-    print(PATHDIR_repository_site)
+    print(
+'''sys.argv=%sys.argv%
+__file__=%__file__%
+PATHFILE_home_user_deploy_site_deploy_site_py=%PATHFILE_home_user_deploy_site_deploy_site_py%
+PATHDIR_home_user_deploy_site=%PATHDIR_home_user_deploy_site%
+PATHDIR_home_user=%PATHDIR_home_user%
+USER=%USER%
+project=%project%
+sitepackage=%sitepackage%
+URL_site=%URL_site%
+PATHDIR_sitepackage=%PATHDIR_sitepackage%
+URL_projectrepository=%URL_projectrepository%
+PATHDIR_repositories=%PATHDIR_repositories%
+PATHDIR_projectrepository=%PATHDIR_projectrepository%
+PATHDIR_projectrepository_sitepackage=%PATHDIR_projectrepository_site%
+PATHFILE_wsgi_py=%PATHFILE_wsgi_py%'''
+        .replace('%sys.argv%', str(sys.argv))
+        .replace('%__file__%', str(__file__))
+        .replace('%PATHFILE_home_user_deploy_site_deploy_site_py%', str(PATHFILE_home_user_deploy_site_deploy_site_py))
+        .replace('%PATHDIR_home_user_deploy_site%', str(PATHDIR_home_user_deploy_site))
+        .replace('%PATHDIR_home_user%', str(PATHDIR_home_user))
+        .replace('%USER%', str(USER))
+        .replace('%project%', str(project))
+        .replace('%sitepackage%', str(sitepackage))
+        .replace('%URL_site%', str(URL_site))
+        .replace('%PATHDIR_sitepackage%', str(PATHDIR_sitepackage))
+        .replace('%PATHDIR_repositories%', str(PATHDIR_repositories))
+        .replace('%URL_projectrepository%', str(URL_projectrepository))
+        .replace('%PATHDIR_projectrepository_sitepackage%', str(PATHDIR_projectrepository_sitepackage))
+        .replace('%PATHFILE_wsgi_py%', str(PATHFILE_wsgi_py))
+    )
 
-    if PATHDIR_repository.is_dir():
-        shutil.rmtree(PATHDIR_repository)
 
-    subprocess.run(['git', 'clone', URL_repository])
+    if PATHDIR_repositories.is_dir():
+        shutil.rmtree(PATHDIR_repositories)
+    PATHDIR_repositories.mkdir(parents=True)
 
-    if PATHDIR_repository.is_dir():
-        shutil.copytree(PATHDIR_repository_site, PATHDIR_site)
-        shutil.rmtree(PATHDIR_repository)
 
+    print('[deploy] Setup project dependencies...')
+
+    print('[deploy] Setup project dependencies!')
+
+
+    print('[deploy] Downloading project repository...')
+    subprocess.run(['git', 'clone', URL_projectrepository])
+    print('[deploy] Downloading project repository!')
+
+    print('[deploy] Install project site...')
+    if PATHDIR_sitepackage.is_dir():
+        shutil.rmtree(PATHDIR_sitepackage)
+
+    if PATHDIR_projectrepository.is_dir():
+        shutil.copytree(PATHDIR_projectrepository_sitepackage, PATHDIR_sitepackage)
+    print('[deploy] Install project site!')
+
+    print('[deploy] Touch wsgi.py...')
     os.system('touch ' + str(PATHFILE_wsgi_py))
+    print('[deploy] Touch wsgi.py!')
+
+    print('[deploy] Deploy site!')
