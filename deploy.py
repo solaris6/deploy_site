59f8c1b097a9e8a3962a0b152ccc5005de7f560a
solaris6 <solaris6@gmail.com>
2019-07-10 12:33:58 +0300

    test

--- deploy.py
+++ deploy.py
@@ -29,22 +29,30 @@ class Sitedeployer:
 
     # common:
     def PATHDIR_home_user_root(self) -> Path:
+        if not self._xxx:
+            yyy
         return self._PATHDIR_home_user_root
 
     def PATHDIR_home_user(self) -> Path:
+        if not self._xxx:
+            yyy
         return self._PATHDIR_home_user
 
     def PATHDIR_root_third(self) -> Path:
+        if not self._xxx:
+            yyy
         return self._PATHDIR_root_third
 
     def PATHDIR_root_repositories(self) -> Path:
+        if not self._xxx:
+            yyy
         return self._PATHDIR_root_repositories
 
     def process_common(self) -> None:
         logger.info('[deployer] Process common...')
 
         logger.info('[deployer] Resolve common paths...')
-        self._PATHDIR_home_user_root = self.PATHDIR_home_user_root_sitedeployer().parent
+        self._PATHDIR_home_user_root = self._PATHFILE_deploypy.parent.parent
         self._PATHDIR_home_user = self.PATHDIR_home_user_root().parent
 
         self._PATHDIR_root_third = self.PATHDIR_home_user_root() / 'third'
@@ -79,12 +87,18 @@ PATHDIR_root_repositories=%PATHDIR_root_repositories%'''
 
     # sitedeployer:
     def PATHFILE_home_user_root_sitedeployer_sitedeployerpackage_deploypy(self) -> Path:
+        if not self._xxx:
+            yyy
         return self._PATHFILE_home_user_root_sitedeployer_sitedeployerpackage_deploypy
 
     def PATHDIR_home_user_root_sitedeployer_sitedeployerpackage(self) -> Path:
+        if not self._xxx:
+            yyy
         return self._PATHDIR_home_user_root_sitedeployer_sitedeployerpackage
 
     def PATHDIR_home_user_root_sitedeployer(self) -> Path:
+        if not self._xxx:
+            yyy
         return self._PATHDIR_home_user_root_sitedeployer
 
     def process_sitedeployer(self) -> None:
@@ -117,12 +131,18 @@ PATHDIR_home_user_root_sitedeployer=%PATHDIR_home_user_root_sitedeployer%'''
         return '''https://github.com/solaris6/base.git'''
 
     def PATHDIR_root_repositories_baserepository(self) -> Path:
+        if not self._xxx:
+            yyy
         return self._PATHDIR_root_repositories_baserepository
 
     def PATHDIR_root_repositories_baserepository_basepackage(self) -> Path:
+        if not self._xxx:
+            yyy
         return self._PATHDIR_root_repositories_baserepository_basepackage
 
     def PATHDIR_root_third_basepackage(self) -> Path:
+        if not self._xxx:
+            yyy
         return self.PATHDIR_root_third() / 'base'
 
 
@@ -157,9 +177,13 @@ PATHDIR_home_user_root_sitedeployer=%PATHDIR_home_user_root_sitedeployer%'''
 
     # project:
     def PATHDIR_root_repositories_projectrepository(self) -> Path:
+        if not self._xxx:
+            yyy
         return self._PATHDIR_root_repositories_projectrepository
 
     def URL_projectrepository(self) -> str:
+        if not self._xxx:
+            yyy
         return '''https://github.com/solaris6/%project_NAME%.git'''\
             .replace('%project_NAME%', self.project_NAME())
 
@@ -195,15 +219,23 @@ PATHDIR_root_repositories_projectrepository=%PATHDIR_root_repositories_projectre
 
     # site:
     def PATHDIR_root_site(self) -> Path:
+        if not self._xxx:
+            yyy
         return self._PATHDIR_root_site
 
     def PATHDIR_root_site_lib(self) -> Path:
+        if not self._xxx:
+            yyy
         return self._PATHDIR_root_site_lib
 
     def PATHDIR_root_site_lib_sitepackage(self) -> Path:
+        if not self._xxx:
+            yyy
         return self._PATHDIR_root_site_lib_sitepackage
 
     def PATHDIR_root_repositories_projectrepository_site(self) -> Path:
+        if not self._xxx:
+            yyy
         return self._PATHDIR_root_repositories_projectrepository_site
 
     def process_site(self) -> None:
@@ -252,25 +284,33 @@ PATHDIR_root_repositories_projectrepository_site=%PATHDIR_root_repositories_proj
         return 'doc_' + self.project_NAME()
 
     def PATHDIR_root_doc(self) -> Path:
+        if not self._xxx:
+        self._PATHDIR_root_doc = self.PATHDIR_home_user_root() / 'doc'
+            yyy
         return self._PATHDIR_root_doc
 
     def PATHDIR_root_doc_lib(self) -> Path:
+        if not self._xxx:
+        self._PATHDIR_root_doc_lib = self.PATHDIR_root_doc() / 'lib'
+            yyy
         return self._PATHDIR_root_doc_lib
 
     def PATHDIR_root_doc_lib_docpackage(self) -> Path:
+        if not self._xxx:
+        self._PATHDIR_root_doc_lib_docpackage = self.PATHDIR_root_doc_lib() / self.docpackage()
+            yyy
         return self._PATHDIR_root_doc_lib_docpackage
 
     def PATHDIR_root_repositories_projectrepository_doc(self) -> Path:
+        if not self._xxx:
+        self._PATHDIR_root_repositories_projectrepository_doc = self.PATHDIR_root_repositories_projectrepository() / 'src/doc'
+            yyy
         return self._PATHDIR_root_repositories_projectrepository_doc
 
     def process_doc(self) -> None:
         logger.info('[deployer] Process doc...')
 
         logger.info('[deployer] Resolve doc paths...')
-        self._PATHDIR_root_doc = self.PATHDIR_home_user_root() / 'doc'
-        self._PATHDIR_root_doc_lib = self.PATHDIR_root_doc() / 'lib'
-        self._PATHDIR_root_doc_lib_docpackage = self.PATHDIR_root_doc_lib() / self.docpackage()
-        self._PATHDIR_root_repositories_projectrepository_doc = self.PATHDIR_root_repositories_projectrepository() / 'src/doc'
 
         logger.info(
 '''docpackage=%docpackage%
@@ -299,16 +339,17 @@ PATHDIR_root_repositories_projectrepository_doc=%PATHDIR_root_repositories_proje
 
     # wsgi.py:
     def PATHFILE_wsgipy(self) -> Path:
+        if not self._xxx:
+            self._PATHFILE_wsgipy = Path(
+                '/var/www/%USER%_pythonanywhere_com_wsgi.py'
+                    .replace('%USER%', self.USER())
+            )
         return self._PATHFILE_wsgipy
 
     def process_wsgipy(self) -> None:
         logger.info('[deployer] Process wsgi.py...')
 
         logger.info('[deployer] Resolve wsgi.py paths...')
-        self._PATHFILE_wsgipy = Path(
-            '/var/www/%USER%_pythonanywhere_com_wsgi.py'
-                .replace('%USER%', self.USER())
-        )
 
         logger.info(
 '''PATHFILE_wsgipy=%PATHFILE_wsgipy%'''
@@ -343,18 +384,19 @@ from %sitepackage%.main import app as application
 
     # update.py:
     def PATHFILE_home_user_root_sitedeployer_sitedeployerpackage_updatepy(self) -> Path:
+        if not self._xxx:
+            self._PATHFILE_home_user_root_sitedeployer_sitedeployerpackage_updatepy = self.PATHDIR_home_user_root_sitedeployer_sitedeployerpackage() / 'update.py'
         return self._PATHFILE_home_user_root_sitedeployer_sitedeployerpackage_updatepy
 
     def PATHFILE_home_user_updatepy(self) -> Path:
+        if not self._xxx:
+            self._PATHFILE_home_user_updatepy = self.PATHDIR_home_user() / 'update.py'
         return self._PATHFILE_home_user_updatepy
 
     def process_updatepy(self) -> None:
         logger.info('[deployer] Process update.py...')
 
         logger.info('[deployer] Resolve update.py paths...')
-        self._PATHFILE_home_user_root_sitedeployer_sitedeployerpackage_updatepy = self.PATHDIR_home_user_root_sitedeployer_sitedeployerpackage() / 'update.py'
-        self._PATHFILE_home_user_updatepy = self.PATHDIR_home_user() / 'update.py'
-
         logger.info(
 '''PATHFILE_home_user_root_sitedeployer_sitedeployerpackage_updatepy=%PATHFILE_home_user_root_sitedeployer_sitedeployerpackage_updatepy%
 PATHFILE_home_user_updatepy=%PATHFILE_home_user_updatepy%'''
@@ -362,7 +404,6 @@ PATHFILE_home_user_updatepy=%PATHFILE_home_user_updatepy%'''
             .replace('%PATHFILE_home_user_updatepy%', str(self.PATHFILE_home_user_updatepy()))
         )
         logger.info('[deployer] Resolve update.py paths!')
-
 
         logger.info('[deployer] Write update.py file...')
         shutil.copyfile(
